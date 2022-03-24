c5ef2c4dac6608c5fdd39c24eea6e676aebae07f
jacepeda <jacepeda@indracompany.com>
2022-03-24 17:36:38 -0500

    cualquiera

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -1,19 +1,33 @@
 import { IUserInfo } from "../components/UserInterface";
+import { encrypt, decrypt } from "../crypto/Encrypter";
 
 export default function useLogin() {
     const baseUrl = "http://localhost:8080/api/login";
 
+    const useCryptography = false; //Switch to encrypt/decrypt data to send
+    
+    const key = "com.symetric.cryptography.spti";
+
     const loginWithUrl = async (data: IUserInfo) => {
-        fetch(`${baseUrl}/url/${data.email}/${data.password}`, {
-            method: "POST",
-        })
+        fetch(
+            `${baseUrl}/url/${
+                useCryptography ? encrypt(data.email, key) : data.email
+            }/${useCryptography ? encrypt(data.password, key) : data.password}`,
+            {
+                method: "POST",
+            }
+        )
             .then((data) => data.json().then((res) => console.log(res)))
             .catch((error) => console.log(error));
     };
 
     const loginWithParams = async (data: IUserInfo) => {
         fetch(
-            `${baseUrl}/params?email=${data.email}&password=${data.password}`,
+            `${baseUrl}/params?email=${
+                useCryptography ? encrypt(data.email, key) : data.email
+            }&password=${
+                useCryptography ? encrypt(data.password, key) : data.password
+            }`,
             {
                 method: "GET",
             }
@@ -25,7 +39,9 @@ export default function useLogin() {
     const loginWithBody = async (data: IUserInfo) => {
         fetch(`${baseUrl}/body`, {
             method: "POST",
-            body: JSON.stringify(data),
+            body: useCryptography
+                ? encrypt(JSON.stringify(data), key)
+                : JSON.stringify(data),
         })
             .then((data) => data.json().then((res) => console.log(res)))
             .catch((error) => console.log(error));
